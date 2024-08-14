3d6318125edb3fb6fae6793846e5923a267cec9b
Nandanibachhane123 <bachhanenandani@gmail.com>
2024-08-14 07:24:08 -0700

    Add files via upload

---
+++ _40array_min.java
@@ -0,0 +1,27 @@
+public class _40array_min
+ {
+    public static void main(String[] args) 
+    {
+        //for each 
+       int arr1[]={2,4,5,1,3,8,};
+       for(int i:arr1){
+       System.out.println(i);
+       /*  int min = arr1[0];//min
+        for(int i = 1;i<arr1.length;i++)
+        {
+            if(min>arr1[i]);
+            {
+            min = arr1[i];
+            }
+
+        }
+
+            
+       }
+        System.out.println(min);*/
+       }
+        
+    }
+}
+    
+
