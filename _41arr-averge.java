39fa8547be0eca13c40e18219a88951c4b89a74b
Nandanibachhane123 <bachhanenandani@gmail.com>
2024-08-17 06:34:35 -0700

    Add files via upload

---
+++ _41arr-averge.java
@@ -0,0 +1,18 @@
+ class averge {
+    public static void main(String[] args)
+     {
+        int arr2 [] = {3,5,8,6,4,2,3,};
+        int sum = 0;
+        int averge;
+        for(int i=0;i<arr2.length;i++)
+        {
+            sum=sum+arr2[i];
+        }
+        averge=sum/arr2.length;
+        System.out.println(averge);
+
+        }
+        
+    }
+    
+
