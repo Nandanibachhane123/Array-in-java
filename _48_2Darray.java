3faaf82ef84dc3eb5af24595ea3efc518a3ea643
Nandanibachhane123 <bachhanenandani@gmail.com>
2024-08-22 19:32:03 -0700

    Add files via upload

---
+++ _48_2Darray.java
@@ -0,0 +1,18 @@
+class Test {
+    public static void main(String[] args) 
+    {
+        int arr [][] = {{1,3,4,},{11,7},{3,5,9,4}};
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr[i].length;j++)
+        {
+                System.out.print(arr[i][j]+" ");     
+        }
+
+        System.out.println();
+
+        }
+
+    }
+
+}
