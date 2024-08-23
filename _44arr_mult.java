365a98cc739d14e1800c6779f250d3eb74f7fea6
Nandanibachhane123 <bachhanenandani@gmail.com>
2024-08-22 19:30:15 -0700

    Add files via upload

---
+++ _44arr_mult.java
@@ -0,0 +1,34 @@
+public class _44arr_mult
+ {
+    public static void main(String[] args)
+    
+    /*multiple of arrya
+     {
+        int arr [] = {2,3,5,6,9};
+        int multiple = 1;
+        for(int i=0;i<arr.length;i++)
+        {
+            multiple = multiple*arr[i];
+        }
+            //multiple=multiple*arr.length;
+            System.out.println(multiple);
+        */
+
+            //Divide in array
+     {
+        int arr [] = {3,4,5,2,6,7};
+        int divide;
+        int sum = 0;
+        for(int i=0;i<arr.length;i++){
+            sum = sum+arr[i];
+        }
+            divide = sum/7;
+            System.out.println(divide);
+
+        }
+           
+        
+    
+}
+    
+
